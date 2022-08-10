e27036c0d06def20c854fcedf383e52b1615fb1c
Tkamath100 <63506620+Tkamath100@users.noreply.github.com>
2022-08-10 20:52:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var randomNumber1=Math.floor((Math.random()*6)+1);
 var randomNumber2=Math.floor((Math.random()*6)+1);
+const img1=document.querySelector(".img1");
 img1.setAttribute("src","images/dice"+randomNumber1+".png");
 const img2=document.querySelector(".img2");
 img2.setAttribute("src","images/dice"+randomNumber2+".png");
